763b62f438ccaf2caa2398e90ba0503460cbdb0f
yusef02 <youssef.bouziane2402@gmail.com>
2024-02-29 22:51:07 +0100

    aggiunte funzioni delle task

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -13,4 +13,21 @@ createApp({
       newTask: { text: "", done: false },
     };
   },
+  methods: {
+    deleteTask(taskIndex) {
+      this.taskList.splice(taskIndex, 1);
+    },
+    addTask(newTask) {
+      if (newTask.text !== "") {
+        this.taskList.push({ ...newTask });
+        newTask.text = "";
+      } else alert("inserire una descrizione per aggiungere la task");
+    },
+    doneTask(taskIndex) {
+      this.taskList[taskIndex].done = true;
+    },
+    redoTask(taskIndex) {
+      this.taskList[taskIndex].done = false;
+    },
+  },
 }).mount("#app");
